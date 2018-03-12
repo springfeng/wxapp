17d69a48498c2576a5bcc84e4c9e136dec497a86
springfeng <spring>
2018-03-12 23:12:17 +0800

    1

--- wxApp/app.js
+++ wxApp/app.js
@@ -49,6 +49,66 @@ App({
               }
             }
           })
+        }else{
+          //没有授权则调用授权界面
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success:res => {
+              // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+              wx.getUserInfo({
+                success: res => {
+                  // 可以将 res 发送给后台解码出 unionId
+
+                  this.globalData.userInfo = res.userInfo
+
+                  // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                  // 所以此处加入 callback 以防止这种情况
+                  if (this.userInfoReadyCallback) {
+                    this.userInfoReadyCallback(res)
+                  }
+                }
+              })
+            },
+            fail:res=>{
+              //授权失败则提示需要授权才能使用
+              wx.showModal({
+                title: '授权失败！',
+                content: '本程序必须授权才能使用',
+                confirmText: "授权",
+                cancelText: "取消",
+                success: function (res) {
+                  if (res.confirm) {
+                    //设置授权操作
+                    wx.openSetting({
+                      success: (res) => {
+                         res.authSetting = {
+                           "scope.userInfo": true,
+                           "scope.userLocation": true
+                         }
+                         
+                         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                         wx.getUserInfo({
+                           success: res => {
+                             // 可以将 res 发送给后台解码出 unionId
+
+                             this.globalData.userInfo = res.userInfo
+
+                             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                             // 所以此处加入 callback 以防止这种情况
+                             if (this.userInfoReadyCallback) {
+                               this.userInfoReadyCallback(res)
+                             }
+                           }
+                         })
+                      }
+                    })
+                  } else {
+                    //取消操作
+                  }
+                }
+              });
+            }
+          })
         }
       }
     })
