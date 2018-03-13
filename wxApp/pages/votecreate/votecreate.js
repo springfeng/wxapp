aa213b7197a50ab0cf303029970618d1b2f9e06d
springfeng <spring>
2018-03-13 23:54:01 +0800

    fixed

--- wxApp/pages/votecreate/votecreate.js
+++ wxApp/pages/votecreate/votecreate.js
@@ -1,7 +1,7 @@
 // pages/voteCreate/votecreate.js
 
 // 在需要使用的js文件中，导入js  
-var util = require('../../utils/util.js');  
+var util = require('../../utils/util.js');
 
 //获取应用实例
 const app = getApp();
@@ -17,12 +17,12 @@ Page({
     disabled: false,
     plain: false,
     loading: false,
-    Begindate:"",
-    BeginTime: "00:00", 
-    Enddate: "", 
+    Begindate: "",
+    BeginTime: "00:00",
+    Enddate: "",
     EndTime: "00:00",
-    checkboxList:[0,1,2],
-    VoteMulti:true,
+    checkboxList: [0, 1, 2],
+    VoteMulti: true,
     LimitTimes: true
   },
 
@@ -30,68 +30,68 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var date = util.formatSingleDate(new Date());  
+    var date = util.formatSingleDate(new Date());
 
     this.setData({
       Begindate: date,
       Enddate: date
     });
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
   /**
    * 用户点击创建按钮
    */
-  createVote: function (e){
-    
+  createVote: function (e) {
+
   },
   /**
    * 多选
@@ -166,51 +166,52 @@ Page({
    */
   formSubmit: function (e) {
     var formData = e.detail.value;
-    var postData={};
+    var postData = {};
     // console.log(formData)
-    postData["VoteItems"]="";
-    var tmp="";
-    for (var item in formData){
-      if (item.indexOf("VoteItems")>=0){
-        tmp+=formData[item]+",";
-      }else{
-        postData[item]=formData[item];
+    postData["VoteItems"] = "";
+    var tmp = "";
+    for (var item in formData) {
+      if (item.indexOf("VoteItems") >= 0) {
+        tmp += formData[item] + ",";
+      } else {
+        postData[item] = formData[item];
       }
     }
     // console.log(tmp)
     // console.log(tmp.substring(0, tmp.length - 1))
     postData["VoteItems"] = tmp.substring(0, tmp.length - 1);
 
+    console.log(JSON.stringify(postData) + '------------')
     //
-    if (postData["VoteMulti"]){
-      postData["VoteMulti"]=1;
-    }else{
-      postData["VoteMulti"]=0;
+    if (postData["VoteMulti"] == "true") {
+      postData["VoteMulti"] = 1;
+    } else {
+      postData["VoteMulti"] = 0;
     }
     //
-    if (postData["LimitTimes"]) {
+    if (postData["LimitTimes"] == "true") {
       postData["LimitTimes"] = 1;
     } else {
       postData["LimitTimes"] = 0;
     }
     //
 
-    postData["OpenID"]=app.globalData.openID;
-    console.log(JSON.stringify(postData) )
+    postData["OpenID"] = app.globalData.openID;
+    console.log(JSON.stringify(postData))
     wx.request({
       url: 'https://www.superiot.vip/api/Vote',
       data: postData,
-      method:"POST",
+      method: "POST",
       header: {
         'Content-Type': 'application/x-www-form-urlencoded' //必须修改才能post成功
       },
       success: function (res) {
-          wx.showToast({
-            title: '创建成功',
-            icon: 'success',
-            duration: 3000
-          });
+        wx.showToast({
+          title: '创建成功',
+          icon: 'success',
+          duration: 3000
+        });
       }
-    })  
+    })
   }
 })
