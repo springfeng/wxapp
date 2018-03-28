511e8d4c3913288e9667c90c934fdb8a285596e4
springfeng <springfeng2008@163.com>
2018-03-28 16:12:30 +0800

    处理统计UI

--- wxApp/pages/Statistics/Statistics.js
+++ wxApp/pages/Statistics/Statistics.js
@@ -1,87 +1,91 @@
-// pages/Statistics/Statistics.js
+import * as echarts from '../../ec-canvas/echarts';
+const app = getApp();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  onShareAppMessage: function (res) {
+    return {
+      title: 'ECharts 可以在微信小程序中使用啦！',
+      path: '/pages/index/index',
+      success: function () { },
+      fail: function () { }
+    }
+  },
   data: {
-  
+    userInfo: {},
+    Vote: {
+      VoteID: '2f43ad5e92234905b1b0b4a65704eea9',
+      VoteTitle: '文字猜谜',
+      OpenID: "oHm4a0ZjlzDfwDP1CFPhge3XdAxM",
+      VoteMulti: 1,
+    },
+    VoteItemsList: [
+      { VoteItemID: "4176f2743561433abcf74049c7dc4e7c", ItemName: "妇女但反对" },
+      { VoteItemID: "4176f2743561433abcf74049c7dc4e7c", ItemName: "543543" }
+    ],
+    ItemSelected: {},
+    OpenID: '',
+    ecBar: {
+      // 如果想要禁止触屏事件，以保证在图表区域内触摸移动仍能滚动页面，
+      // 就将 disableTouch 设为 true
+      // disableTouch: true,
+
+      onInit: initChart
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    wx.hideShareMenu();
-    // console.log("接收到的参数是str=" + options.VoteID + "-" + options.OpenID); 
-    var self = this;
-
-    //获取投票详情
-    wx.request({
-      url: 'https://www.superiot.vip/api/VoteDetail/?OpenID=' + options.OpenID + '&VoteID=' + options.VoteID + '&rn=' + Math.random(),
-      method: "GET",
-      header: {
-        'Content-Type': 'application/x-www-form-urlencoded' //必须修改才能post成功
-      },
-      success: function (res) {
-        console.log(res);
-        self.setData({
-          Vote: res.data,
-          VoteItemsList: res.data.VoteItemsList,
-          ItemSelected: {},
-          OpenID: options.OpenID,
-          VoteID: options.VoteID
-        });
-      }
+  onReady() {
+    this.setData({
+      userInfo: app.globalData.userInfo
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
-})
+});
+
+function initChart(canvas, width, height) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chart);
+
+  var option = {
+    backgroundColor: "#ffffff",
+    color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
+    series: [{
+      label: {
+        normal: {
+          fontSize: 14
+        }
+      },
+      type: 'pie',
+      center: ['50%', '50%'],
+      radius: [0, '60%'],
+      data: [{
+        value: 55,
+        name: '北京'
+      }, {
+        value: 20,
+        name: '武汉'
+      }, {
+        value: 10,
+        name: '杭州'
+      }, {
+        value: 20,
+        name: '广州'
+      }, {
+        value: 38,
+        name: '上海'
+      },
+      ],
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 2, 2, 0.3)'
+        }
+      }
+    }]
+  };
+
+  chart.setOption(option);
+  return chart;
+}
